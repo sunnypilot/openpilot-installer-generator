94237d369583919f2301398609227ba00cfe3e41
Jason Wen <haibin.wen3@gmail.com>
2025-04-02 01:33:41 -0400

    Reapply "default name"
    This reverts commit 09745fe40024d41a669e8997a1f600c2da4862b1.

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ if (array_key_exists("url", $_GET)) {
 $parts = explode("/", $url);
 $branch = isset($parts[0]) ? $parts[0] : "";
 $loading_msg = isset($parts[1]) ? $parts[1] : "";
-$username = "sunnypilot";
+$username = "sunnyhaibin";
 
 $branch = substr(trim($branch), 0, 255);  # max GH branch
 $branch = $branch == "_" ? "" : $branch;
@@ -101,7 +101,7 @@ button:active[name="download_agnos"] {border-radius: 4px; border: 5px; padding: 
 echo '</br></br><a href="' . BASE_DIR . '"><h1 style="color: #30323D;">🍴 openpilot fork installer generator-inator 🍴</h1></a>';
 echo '<h3 style="position: absolute; bottom: 0; left: 0; width: 100%; text-align: center;"><a href="https://github.com/sshane/openpilot-installer-generator" style="color: 30323D;">💾 Installer Generator GitHub Repo</a></h3>';
 
-if ($username == "sunnypilot" and $branch == "") {
+if ($username == "sunnyhaibin" and $branch == "") {
     echo '<h3 style="color: #30323D;">🎉 now supports comma three! 🎉<h3>';
     echo "</br><h2>Enter this URL on your device during setup with the format:</h2>";
     echo "<h2><a href='" . BASE_DIR . "/release-c3'><span>" . WEBSITE_URL . BASE_DIR . "/branch</span></a></h2>";
